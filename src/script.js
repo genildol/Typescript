9032db089269250d78cfee9b5009b97331c9f96f
Genildo Lopes <jgmicroshs@gmail.com>
2025-04-30 23:53:32 -0300

    config typs projetc

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 var numero1 = document.getElementById('numero1');
 var numero2 = document.getElementById('numero2');
-var botao = document.getElementById('calcularas');
+var botao = document.getElementById('calcular');
 var res = document.getElementById('resultado');
 function calcular(n1, n2) {
     return n1 + n2;
 }
 botao.addEventListener('click', function () {
-    res.innerHTML = calcular(+numero1.value, +numero2.value).toString();
+    res.innerHTML = calcular(numero1.value, numero2.value);
 });
