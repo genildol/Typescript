dad53dd620498d02d283b25e9d84034d5d04d12b
Genildo Souza <jgmicroshs@gmail.com>
2025-07-27 16:53:59 -0300

    Optional Properties

--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,12 @@
-function resumo(usuario: { nome: string, idade: number }) {
-  return 'Ola, $(usuario.nome). Você tem $(usuario.idade) anos.';
+function resumo(usuario: { nome: string, idade?: number }) {
+  if (usuario.idade !== undefined) {
+    return 'Ola, $(usuario.nome). Você tem $(usuario.idade) anos.';
+  } else {
+  } return 'Ola, $(usuario.nome). Você não informou a idade.';
 }
-
 let u = {
   nome: 'Genildo',
   idade: 50,
 };
 
-console.log(resumo(u));
+resumo(u);
