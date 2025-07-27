7e5baddf091dcb549ee140a9da4c34bc0c3594d6
Genildo Souza <jgmicroshs@gmail.com>
2025-07-27 16:37:47 -0300

    Contextual Typing in Anonymous Functions

--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,9 @@
+let nomes = ['joão', 'maria', 'josé', 'ana', 'pedro', 90];
 
-let nomes: any[] = ['Genildo', 'Mario', 'Lauro'];
-
-
-nomes.push(100);
-
-
-// let nome: string = 'Genildo';
-
-// let idade: number = 100;
-
-
+nomes.forEach(function (nome) {
+  if (typeof nome === 'string') {
+    console.log(nome.toUpperCase());
+  } else {
+    console.log(nome);
+  }
+});
