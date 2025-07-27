f19cacff41856512d0ec940965bf3079ef795c87
Genildo Souza <jgmicroshs@gmail.com>
2025-07-27 14:48:24 -0300

    Primitive types

--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,7 @@
-let numero1 = document.getElementById("numero1") as HTMLInputElement;
-let numero2 = document.getElementById("numero2") as HTMLInputElement;;
-let botao = document.getElementById("calcular");
-let res = document.getElementById("resultado");
+let nome: string = 'Genildo';
 
-function calcular(n1: number, n2: number) {
-  return n1 + n2;
-}
+let idade: number = 100;
 
-botao.addEventListener("click", function () {
-  res.innerHTML = calcular(+numero1.value, +numero2.value).toString();
+let programador: boolean = true;
 
-});
+
