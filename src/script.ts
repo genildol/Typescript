3b2dcf396ea86e85da3b4b7504949e94c4ebdd13
Genildo Souza <jgmicroshs@gmail.com>
2025-07-27 15:04:52 -0300

    Type em Arrays

--- src/script.ts
+++ src/script.ts
@@ -4,4 +4,10 @@ let idade: number = 100;
 
 let programador: boolean = true;
 
+let nomes: string[] = ['Genildo', 'Mario', 'Lauro'];
 
+let programadores: Array<string> = ['Genildo', 'Mario', 'Lauro'];
+
+let idades: number[] = [10, 20, 30];
+
+let numeros: Array<number> = [1, 2, 3];
