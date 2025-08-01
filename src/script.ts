22b3aa5d9864694d7c4c6a62d3ddde218e1cbc12
Genildo Lopes <jgmicroshs@gmail.com>
2025-07-31 23:05:19 -0300

    Type Assertions and Types literais

--- src/script.ts
+++ src/script.ts
@@ -1,21 +1,59 @@
-// type User = {
-//   nome: string;
-//   idade: number;
-// };
-
-interface User {
-  nome: string;
-  idade: number;
-}
-interface User {
-  email: string;
+type Opcoes = {
+  width: number;
+  heigth: number;
 }
 
-function resumo(usuario: User) {
-  return 'Ola, ${usuario.nome}, voce tem ${usuario.idade} anos.';
+// function configurar(props: { width: number, heigth: number } | 'auto') {
+
+// }
+function configurar(props: Opcoes | 'auto') {
+
 }
-resumo({
-  nome: 'Genildo',
-  idade: 90,
-  email: 'gsmicros@gsmicros.com'
-});
+
+configurar({ width: 100, heigth: 200 });
+configurar('auto');
+// configurar('automatico');
+
+
+
+/* TypeScript é um superconjunto do JavaScript que adiciona tipagem estática opcional e outros recursos avançados.
+   - Ele é compilado para JavaScript puro, o que significa que pode ser executado
+*/
+
+// type VerdadeFalso = true | false;
+
+// function temNome(nome: string): true | false {
+//   if (nome !== 'genildo') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
+/*
+// TypeScript permite a sobrecarga de tipos,
+*/
+
+// function mostrarTexto(
+//   texto: string,
+//   alinhamento: 'left' | 'center' | 'right'
+// ) {
+//   return `<p style="text-align: ${alinhamento}">${texto}</p>`;
+// }
+
+// mostrarTexto('Ola, mundo!', 'center');
+// mostrarTexto('Ola, mundo!', 'left');
+// mostrarTexto('Ola, mundo!', 'teste');
+
+
+
+
+
+/* 
+// TypeScript permite a sobrecarga de interfaces,
+
+*/
+// let idadeField = document.getElementById('idade');
+let idadeField = document.getElementById('idade') as HTMLInputElement;
+console.log(idadeField.value);
