ee21841d8b48d2e1c4409a517b5fc2a4e889ac92
Genildo Lopes <jgmicroshs@gmail.com>
2025-08-02 10:14:54 -0300

    inferencia literal

--- src/script.ts
+++ src/script.ts
@@ -1,59 +1,41 @@
-type Opcoes = {
-  width: number;
-  heigth: number;
-}
-
-// function configurar(props: { width: number, heigth: number } | 'auto') {
-
-// }
-function configurar(props: Opcoes | 'auto') {
-
-}
-
-configurar({ width: 100, heigth: 200 });
-configurar('auto');
-// configurar('automatico');
-
-
-
-/* TypeScript é um superconjunto do JavaScript que adiciona tipagem estática opcional e outros recursos avançados.
-   - Ele é compilado para JavaScript puro, o que significa que pode ser executado
+/*
+TYPES PARA FUNÇÕES
 */
+type MainFunction =
 
-// type VerdadeFalso = true | false;
 
-// function temNome(nome: string): true | false {
-//   if (nome !== 'genildo') {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
-// TypeScript permite a sobrecarga de tipos,
+INFEENCIA LITERAL
 */
 
-// function mostrarTexto(
-//   texto: string,
-//   alinhamento: 'left' | 'center' | 'right'
-// ) {
-//   return `<p style="text-align: ${alinhamento}">${texto}</p>`;
+// function fazerRequisicao(url: string, method: 'GET' | 'POST') {
+//   //.....
 // }
 
-// mostrarTexto('Ola, mundo!', 'center');
-// mostrarTexto('Ola, mundo!', 'left');
-// mostrarTexto('Ola, mundo!', 'teste');
+// type RequestDetails = {
+//   url: string,
+//   method: 'GET' | 'POST';
+// }
 
+// let req: RequestDetails = {
+//   url = 'https://www.google.com',
+//   method = 'GET'
 
+// };
 
+// req.method = 'POST';
 
-
-/* 
-// TypeScript permite a sobrecarga de interfaces,
-
-*/
-// let idadeField = document.getElementById('idade');
-let idadeField = document.getElementById('idade') as HTMLInputElement;
-console.log(idadeField.value);
+// fazerRequisicao(req.url, req.method);
